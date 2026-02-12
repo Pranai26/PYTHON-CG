302307db7a7c7b9b233d791fb5acf56b49467e90
Pranai Vummadisett <vpranai67@gmail.com>
2026-02-13 00:04:02 +0530

    Add files via upload
    This is my first day in codegnag and today i learnt about the introduction of python

---
+++ Day-1.py
@@ -0,0 +1,45 @@
+# Day 1 Practice File
+
+print("Welcome to CodeGyan!")
+print("This is my first day.")
+print("Today i learnt about Python Introduction - Day 1.")
+print("\n")
+
+print("Python is a simple & is easy to learn & widely used in Web Development.")
+print("Python was created by Dutch programmer Guido van Rossum")
+print("\n")
+
+print("----- STRINGS IN PYTHON -----")
+
+name = "Pranai"
+course = "Python Fullstack"
+
+print("My name is:", name)
+print("I am learning:", course)
+
+message = "Hello " + name + ", welcome to " + course
+print(message)
+
+print("Length of name:", len(name))
+print("First character in name:", name[0])
+print("Last character in name:", name[-1])
+
+print("\n")
+
+print("----- ARITHMETIC OPERATIONS -----")
+
+x = 10
+y = 5
+
+print("Value of x:", x)
+print("Value of y:", y)
+
+print("Add (x + y):", x + y)
+print("Sub (x - y):", x - y)
+print("Multipli (x * y):", x * y)
+print("Div (x / y):", x / y)
+print("Mod (x % y):", x % y)
+
+
+print("\n")
+print("End")
