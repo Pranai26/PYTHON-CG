f99fb079de5b875a5634d281009055aae24d2ccb
Pranai Vummadisett <vpranai67@gmail.com>
2026-02-14 23:10:52 +0530

    Add files via upload
    Day 3 in codegnan and today i learnt about Operators & Condition Statements

---
+++ Day-3(Operators & Condition Statements).py
@@ -0,0 +1,33 @@
+"""#check is number is zero
+num = (int(input()))
+if(num==0):
+    print("num is zero")
+print("program done")"""
+
+
+"""#check is number is zero or not
+num = (int(input()))
+if(num==0):
+    print("number is zero")
+else:
+    print("number is not zero")"""
+
+
+"""num = (int(input()))
+if(num!=0):
+    print("number is not zero")
+else:
+    print("number is zero")"""
+
+
+#check which number is bigger
+a = (int(input()))
+b = (int(input()))
+if(a>b):
+    print("a is the bigger number")
+else:
+    print("b is the bigger number")
+if(a<b):
+    print("a is the smaller number")
+else:
+    print("b is the smaller number")
